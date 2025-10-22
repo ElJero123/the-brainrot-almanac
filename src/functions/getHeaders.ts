aace0e6e3365a065a3a5f8154af070e3d10041e2
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-10-22 17:23:43 -0500

    testing headers

--- src/functions/getHeaders.ts
+++ src/functions/getHeaders.ts
@@ -2,7 +2,7 @@ export async function getHeaders({ request }: { request : Request }) {
     const spanishCountries = ['co', 'ar', 'bo', 'cl', 'cr', 'cu', 'do', 'ec', 
     'sv', 'es', 'gt', 'hn', 'mx', 'ni', 'pa', 'py', 'pe', 'pr', 'uy', 've', 'gq']
 
-    const headerIPUbication = request.headers.get('x-vercel-ip-country')?.toLowerCase()
+    const headerIPUbication = request.headers.get('x-vercel-ip-country')?.toUpperCase()
     const isUbication = spanishCountries.filter(country => country === headerIPUbication)
 
     return isUbication[0] !== headerIPUbication
--- src/pages/api/headers.ts
+++ src/pages/api/headers.ts
@@ -1,7 +1,9 @@
-export const GET = ({ request }: { request: Request }) => {
-  const headers = Object.fromEntries(request.headers.entries())
-  
-  return new Response(JSON.stringify(headers, null, 2), {
-    headers: { "Content-Type": "application/json" },
-  });
-};
+// testing
+
+// export const GET = ({ request }: { request: Request }) => {
+//   const headers = Object.fromEntries(request.headers.entries())
+
+//   return new Response(JSON.stringify(headers, null, 2), {
+//     headers: { "Content-Type": "application/json" },
+//   })
+// };
