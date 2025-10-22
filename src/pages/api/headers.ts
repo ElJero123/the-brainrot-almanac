f40a370fd0a7f78ad9b46aca9418051426e4dbad
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-10-22 17:15:19 -0500

    Testing headers

---
+++ src/pages/api/headers.ts
@@ -0,0 +1,7 @@
+export const GET = ({ request }: { request: Request }) => {
+  const headers = Object.fromEntries(request.headers.entries())
+  
+  return new Response(JSON.stringify(headers, null, 2), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
