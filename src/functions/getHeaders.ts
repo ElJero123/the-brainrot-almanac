2e259e533f1fbc271c7d0d02bace36a4f4fcc0e5
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-11-05 12:44:36 -0500

    Testing headers 3

--- src/functions/getHeaders.ts
+++ src/functions/getHeaders.ts
@@ -2,10 +2,10 @@ export async function getHeaders({ request }: { request : Request }) {
     const spanishCountries = ['co', 'ar', 'bo', 'cl', 'cr', 'cu', 'do', 'ec', 
     'sv', 'es', 'gt', 'hn', 'mx', 'ni', 'pa', 'py', 'pe', 'pr', 'uy', 've', 'gq']
 
-    const headerIPUbication = request.headers.get('x-vercel-ip-country') as string// CO
+    const headerIPUbication = request.headers.get('x-vercel-ip-country') // CO
     const isUbication = spanishCountries.filter(country => country.toUpperCase() === headerIPUbication)
     
     // CO
 
-    return isUbication[0] === headerIPUbication.toLowerCase()
+    return isUbication[0].toUpperCase() === headerIPUbication
 }
