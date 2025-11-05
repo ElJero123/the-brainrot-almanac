f1e2c5cbe23bb2792920dd2829d0a89034a78e4a
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-11-05 12:32:46 -0500

    Adding logs to header route

--- src/functions/getHeaders.ts
+++ src/functions/getHeaders.ts
@@ -2,8 +2,12 @@ export async function getHeaders({ request }: { request : Request }) {
     const spanishCountries = ['co', 'ar', 'bo', 'cl', 'cr', 'cu', 'do', 'ec', 
     'sv', 'es', 'gt', 'hn', 'mx', 'ni', 'pa', 'py', 'pe', 'pr', 'uy', 've', 'gq']
 
-    const headerIPUbication = request.headers.get('x-vercel-ip-country')?.toUpperCase()
+    const headerIPUbication = request.headers.get('x-vercel-ip-country')?.toUpperCase() // CO
     const isUbication = spanishCountries.filter(country => country.toUpperCase() === headerIPUbication)
+    console.log('headerIPUbication', headerIPUbication);
+    console.log('isUbication', isUbication);
+    
+    // CO
 
     return isUbication.includes(headerIPUbication!)
 }
