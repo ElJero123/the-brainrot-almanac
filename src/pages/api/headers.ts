b41cbcb806c15a3155d6b4846c3aa8da52cbf63a
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-11-05 12:49:06 -0500

    Removing the route headers

--- src/pages/api/headers.ts
+++ src/pages/api/headers.ts
@@ -1,7 +1,8 @@
-export const GET = ({ request }: { request: Request }) => {
-  const headers = Object.fromEntries(request.headers.entries())
+// testing
+// export const GET = ({ request }: { request: Request }) => {
+//   const headers = Object.fromEntries(request.headers.entries())
 
-  return new Response(JSON.stringify(headers, null, 2), {
-    headers: { "Content-Type": "application/json" },
-  })
-};
+//   return new Response(JSON.stringify(headers, null, 2), {
+//     headers: { "Content-Type": "application/json" },
+//   })
+// };
